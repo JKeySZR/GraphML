735ade3bed117d1bd32e77ce76a407de34dff6d3
JKey <MBochar0ff@gmail.com>
2015-08-24 09:53:02 +0400

    Start analyze VIEWS

--- dxray/drupal.php
+++ dxray/drupal.php
@@ -17,7 +17,7 @@ function dxray_debug_stdout($msg, $type = DXRAY_MSG_STATUS) {
       echo $msg . PHP_EOL;
       break;
     case DXRAY_MSG_DEBUG:
-      echo "[DEBUG] " .  var_dump(debug_backtrace()) . $msg . PHP_EOL;
+      echo "[DEBUG] " . var_dump(debug_backtrace()) . $msg . PHP_EOL;
       break;
 
     default:
@@ -34,12 +34,12 @@ function dxray_debug_stdout($msg, $type = DXRAY_MSG_STATUS) {
  */
 function dxray_get_node_type() {
   $GML = new GraphML();
-  
+
   $optN1 = $GML->setOptionsNode()
-    ->NodeFill_setColor('#ccffff')
-    ->NodeLabel_setFontSize('22')
-    ->getOptions();
-  
+      ->Fill_setColor('#ccffff')
+      ->Label_setFontSize('22')
+      ->getOptions();
+
   $aNtypes = node_type_get_types();
   foreach ($aNtypes as $oType) {
     // 1. Сначала создаем узел NODE_TYPE 
@@ -58,8 +58,8 @@ function dxray_get_node_type() {
     );
     $options['NodeFill']['color'] = '#ccffff';
     $ID_bundle = $GML->addNode($oType->name, 'UMLClassNode', $optN1, $data);
-    
-    dxray_debug_stdout('Добавили bundle номер: ' . $ID_bundle );
+
+    dxray_debug_stdout('Добавили bundle номер: ' . $ID_bundle);
     // 2. Получаем поля данного контента и строем зависимости
     $fields = field_info_instances('node', $oType->type);
     foreach ($fields as $field) {
@@ -86,8 +86,8 @@ function dxray_get_node_type() {
 
 
       $ID_field = $GML->addNode($field['field_name'], 'UMLClassNode', null, $dataHTML);
-      dxray_debug_stdout('Добавили field номер: ' . $ID_field );
-      dxray_debug_stdout("{EDGE} SRC: $ID_bundle TARGET: $ID_field " . $ID_field );
+      dxray_debug_stdout('Добавили field номер: ' . $ID_field);
+      dxray_debug_stdout("{EDGE} SRC: $ID_bundle TARGET: $ID_field " . $ID_field);
       $GML->addEdge($ID_bundle, $ID_field);
     }
   }
@@ -137,10 +137,10 @@ function _dxray_parse_bundles(&$IDFIELD, &$bundles, &$REID, &$GML) {
       $ID = $GML->addNode($entity, 'ShapeNode', $options);
       $REID['nodes'][$entity] = $ID;
     }
-    
+
     $edge = new GML_EDGEOPT;
     $edge->LineStyle_setColor('#ffaaff');
-    
+
     // 2. Шаг второй перебираем ипостаси этой сущности
     foreach ($items as $name) {
       if (!isset($REID['nodes'][$name])) {
@@ -153,7 +153,7 @@ function _dxray_parse_bundles(&$IDFIELD, &$bundles, &$REID, &$GML) {
       $idBundle = $REID['nodes'][$name];
       // Проверяем есть ли уже такая связь между сущностью и эпостасью
       if (!isset($REID['edges'][$idEntity][$idBundle])) {
-        $GML->addEdge($idEntity, $idBundle,'',$edge->getOptions());
+        $GML->addEdge($idEntity, $idBundle, '', $edge->getOptions());
         $REID['edges'][$idEntity][$idBundle] = 1;
       }
       // Проверяем есть ли уже такая связь между эпостасью и ее полями
@@ -163,4 +163,98 @@ function _dxray_parse_bundles(&$IDFIELD, &$bundles, &$REID, &$GML) {
       }
     }
   }
+}
+
+function dxray_convert_bool($bool){
+  $val = '';
+  switch ($bool) {
+    case TRUE:      
+      $val = 'True';    
+      break;
+    case FALSE:      
+      $val = 'False';    
+      break;
+    
+    default:
+      break;
+  }
+  return $val;
+  
+}
+
+/**
+ *  Получить список VIEWS
+ */
+function dxray_get_views() {
+  $GML = new GraphML();
+  $VIEWS = views_get_all_views();
+
+  $nodeView = $GML->setOptionsNode()
+      ->Fill_setColor('#ccffff')
+      ->getOptions();
+
+  $nodeDisplay = $GML->setOptionsNode()
+      ->Shape_setType('ellipse')
+      ->Fill_setColor('#ccffcc')
+      ->getOptions();
+  
+  $nodeDisplayPlug = $GML->setOptionsNode()
+      ->Shape_setType('parallelogram')
+      ->Fill_setColor('#33cccc')
+      ->getOptions();
+  
+  foreach ($VIEWS as $view) {
+    //$id_view = $GML->addNodeShape($view->human_name);
+    $v_data['attributes'] = array(
+      'Human name: ' . $view->human_name,
+      'Base_table: ' . $view->base_table,
+      'Base_field: ' . $view->base_field,
+      'Built: ' . dxray_convert_bool($view->built), // boolean
+      'Executed: ' . dxray_convert_bool($view->executed), // boolean
+      'Editing: ' . dxray_convert_bool($view->executed), // boolean
+      'Type: ' . $view->type, // string
+    );
+            
+    if($view->attachment_before != ''){
+      $v_data['attributes'][] = 'Attachment before: ' . $view->attachment_before; // string  
+    }
+    
+    if($view->attachment_after != ''){
+      $v_data['attributes'][] = 'Attachment after: ' . $view->attachment_after; // string  
+    }    
+    
+    //$id_view = $GML->addNodeUMLClass($view->name, $v_data, $nodeView);
+    $id_view = $GML->addNode($view->name, 'UMLClassNode', $nodeView, $v_data);
+    $id_display = $GML->addNodeShape('Display', $nodeDisplay);
+    $GML->addEdge($id_view, $id_display);
+
+    $id_disp_plug_type = array(); // name -> GML_ID
+
+    foreach ($view->display as $display) {
+      if (!isset($id_disp_plug_type[$display->display_plugin])) {
+        $id_display_type = $GML->addNodeShape($display->display_plugin, $nodeDisplayPlug);        
+        $GML->addEdge($id_display, $id_display_type);
+        $id_disp_plug_type[$display->display_plugin] = $id_display_type;
+      }else{
+        $id_display_type = $id_disp_plug_type[$display->display_plugin];
+      }
+
+      $data['attributes'] = array(
+        'ID: ' . $display->id,
+        'Display_Title: ' . $display->display_title,
+        'Display_plugin: ' . $display->display_plugin,
+        'DB table: ' . $display->db_table,
+      );
+
+      if (isset($display->display_options['path'])) {
+        $data['attributes'][] = 'Path: ' . $display->display_options['path'];
+      }
+
+      $id = $GML->addNodeUMLClass($display->display_title, $data);
+      $GML->addEdge($id_display_type, $id);
+    }
+  }
+  $dbg = 0;
+  $file = DXRAY_OUTPATH . '/Views-' . date('d-m-Y_H-i-s') . '.graphml';
+  $GML->createFullGraphML($file);
 }
